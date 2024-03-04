199819447936dcec5651b8738a9a97815b493695
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-03-03 20:34:59 -0500

    Provide some examples for few flet tryout

---
+++ basic_app_advanced.py
@@ -0,0 +1,31 @@
+import flet as ft
+
+def main(page):
+    def add_clicked(e):
+        page.add(ft.Checkbox(label=new_task.value))
+        new_task.value = ""
+        new_task.focus()
+        new_task.update()
+
+    new_task = ft.TextField(hint_text="Whats needs to be done?", width=300)
+    page.add(ft.Row([new_task, ft.ElevatedButton("Add", on_click=add_clicked)]))
+
+    #visible property
+    first_name = ft.TextField()
+    last_name = ft.TextField()
+    # first_name.disabled = True
+    # last_name.disabled = True
+    page.add(first_name, last_name)
+
+    #disabled property
+    first_name = ft.TextField()
+    last_name = ft.TextField()
+    c = ft.Column(controls=[
+        first_name,
+        last_name
+    ])
+    c.disabled = True
+    page.add(c)
+
+
+ft.app(target=main)
